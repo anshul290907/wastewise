ced3eccf6df79c8d9839250ac0af37551b63a691
anshul290907 <anshul290907@gmail.com>
2026-09-19 08:35:17 +0530

    Fix Vercel API entrypoint

--- api/[...path].ts
+++ api/[...path].ts
@@ -1,4 +1,25 @@
-// @ts-ignore
-import app from "../server/vercel.ts";
+import "dotenv/config";
+import express from "express";
+import { createExpressMiddleware } from "@trpc/server/adapters/express";
+import { registerGoogleOAuthRoutes } from "../server/auth/google";
+import { registerStorageProxy } from "../server/_core/storageProxy";
+import { appRouter } from "../server/routers";
+import { createContext } from "../server/_core/context";
+
+const app = express();
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
+registerStorageProxy(app);
+registerGoogleOAuthRoutes(app);
+
+app.use(
+  "/api/trpc",
+  createExpressMiddleware({
+    router: appRouter,
+    createContext,
+  })
+);
 
 export default app;
